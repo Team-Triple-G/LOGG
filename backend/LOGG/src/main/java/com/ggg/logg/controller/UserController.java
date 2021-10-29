bc16d8cb788e60823b8afa2cf95997b94acb23e6
cherrytomato1 <terec@naver.com>
2021-10-29 20:41:02 +0900

    fix: 크로스 오리진 설정 추가

--- backend/LOGG/src/main/java/com/ggg/logg/controller/UserController.java
+++ backend/LOGG/src/main/java/com/ggg/logg/controller/UserController.java
@@ -8,11 +8,13 @@ import com.ggg.logg.model.response.user.UserLoginResponse;
 import com.ggg.logg.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
